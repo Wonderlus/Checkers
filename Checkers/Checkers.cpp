8802ee8f16b22fe0a11380103152d84a29e4cf90
Wonderlus <ilya.jiltzov@gmail.com>
2023-05-03 23:33:02 +0300

    added moves, changed interface

--- Checkers/Checkers.cpp
+++ Checkers/Checkers.cpp
@@ -10,6 +10,7 @@ struct Cell {
 	int y;
 	int xEnd;
 	int yEnd;
+	int checker;
 };
 
 struct Checker {
@@ -28,6 +29,7 @@ int field[8][8];
 
 
 
+
 enum ConsoleColors {
 	Black = 0,
 	White = 1,
@@ -41,9 +43,24 @@ enum ConsoleColors {
 
 COORD dot;
 
-HDC cyan, white, black, grey, blue, red;
+HDC cyan, white, black, grey, blue, red, yellow, brown, silver;
 
+void drawCursor() {
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			if (field[i][j] == 1) {
+				Rectangle(silver, cells[i][j].x, cells[i][j].y, cells[i][j].xEnd, cells[i][j].yEnd);
+				if (cells[i][j].checker == 1) {
+					RoundRect(brown, cells[i][j].x, cells[i][j].y, cells[i][j].xEnd, cells[i][j].yEnd, 90, 90);
+				}
+				else if (cells[i][j].checker == 2) {
+					RoundRect(white, cells[i][j].x, cells[i][j].y, cells[i][j].xEnd, cells[i][j].yEnd, 90, 90);
+				}
+			}
 
+		}
+	}
+}
 void draw() {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	Rectangle(grey, 0, 0, 1200, 1200);
@@ -54,21 +71,133 @@ void draw() {
 	for (int i = 0; i < 8; i++) {
 		for (int j = 0; j < 8; j++) {
 			if ((i + j) % 2 == 0) {
-				Rectangle(white, cells[i][j].x, cells[i][j].y, cells[i][j].xEnd, cells[i][j].yEnd);
+				Rectangle(yellow, cells[i][j].x, cells[i][j].y, cells[i][j].xEnd, cells[i][j].yEnd);
+				
 			}
 			else {
 				Rectangle(black, cells[i][j].x, cells[i][j].y, cells[i][j].xEnd, cells[i][j].yEnd);
-				if (field[i][j] == 1) {
-					RoundRect(red, cells[i][j].x, cells[i][j].y, cells[i][j].xEnd, cells[i][j].yEnd, 90, 90);
+				
+				if (cells[i][j].checker == 1) {
+					RoundRect(brown, cells[i][j].x, cells[i][j].y, cells[i][j].xEnd, cells[i][j].yEnd, 90, 90);
 				}
-				else if (field[i][j] == 2) {
-					RoundRect(blue, cells[i][j].x, cells[i][j].y, cells[i][j].xEnd, cells[i][j].yEnd, 90, 90);
+				else if (cells[i][j].checker == 2) {
+					RoundRect(white, cells[i][j].x, cells[i][j].y, cells[i][j].xEnd, cells[i][j].yEnd, 90, 90);
 				}
 			}
+			
 		}
 	}
 }
 
+
+void move() {
+
+	while (TRUE) {
+		// ВНИЗ - 80, ВВЕРХ - 72, ВПРАВО - 77, ВЛЕВО - 75, ENTER - 13, ESC - 27.
+		int curMove = _getch();
+		if (curMove == 80) {
+			bool flag = false;
+			for (int i = 0; i < 8; i++) {
+				for (int j = 0; j < 8; j++) {
+					if (field[i][j] == 1) {
+						if (i < 7) {
+							field[i][j] = 0;
+							field[i + 1][j] = 1;
+						}
+						flag = true;
+						break;
+					}
+				}
+				if (flag) {
+					break;
+				}
+			}
+
+			draw();
+			drawCursor();
+		}
+
+		if (curMove == 72) {
+			bool flag = false;
+			for (int i = 0; i < 8; i++) {
+				for (int j = 0; j < 8; j++) {
+					if (field[i][j] == 1) {
+						if (i > 0) {
+							field[i][j] = 0;
+							field[i - 1][j] = 1;
+						}
+						flag = true;
+						break;
+					}
+				}
+				if (flag) {
+					break;
+				}
+			}
+
+			draw();
+			drawCursor();
+		}
+
+
+
+		if (curMove == 77) {
+			bool flag = false;
+			for (int i = 0; i < 8; i++) {
+				for (int j = 0; j < 8; j++) {
+					if (field[i][j] == 1) {
+						if (j < 7) {
+							field[i][j] = 0;
+							field[i][j + 1] = 1;
+						}
+						flag = true;
+						break;
+					}
+				}
+				if (flag) {
+					break;
+				}
+			}
+
+			draw();
+			drawCursor();
+		}
+
+		if (curMove == 75) {
+			bool flag = false;
+			for (int i = 0; i < 8; i++) {
+				for (int j = 0; j < 8; j++) {
+					if (field[i][j] == 1) {
+						if (j > 0) {
+							field[i][j] = 0;
+							field[i][j - 1] = 1;
+						}
+						flag = true;
+						break;
+					}
+				}
+				if (flag) {
+					break;
+				}
+			}
+
+			draw();
+			drawCursor();
+		}
+
+		if (curMove == 13) {
+
+		}
+
+		if (curMove == 27) {
+			system("cls");
+			cin.ignore();
+			break;
+		}
+	}
+	
+
+}
 
 int main() {
 	
@@ -88,15 +217,23 @@ int main() {
 	SelectObject(black, CreateSolidBrush(RGB(0, 0, 0)));
 	
 	grey = GetDC(GetConsoleWindow());
-	SelectObject(grey, CreateSolidBrush(RGB(192, 192, 192)));
+	SelectObject(grey, CreateSolidBrush(RGB(211, 211, 211)));
+
+	silver = GetDC(GetConsoleWindow());
+	SelectObject(silver, CreateSolidBrush(RGB(192, 192, 192)));
 
 	blue = GetDC(GetConsoleWindow());
 	SelectObject(blue, CreateSolidBrush(RGB(0, 0, 255)));
 
 	red = GetDC(GetConsoleWindow());
 	SelectObject(red, CreateSolidBrush(RGB(255, 0, 0)));
-	
-	
+	yellow = GetDC(GetConsoleWindow());
+	SelectObject(yellow, CreateSolidBrush(RGB(237, 171, 86)));
+
+	brown = GetDC(GetConsoleWindow());
+	SelectObject(brown, CreateSolidBrush(RGB(115, 74, 18)));
+
+	// Заполнение координат и цветов для клеток
 	for (int i = 0; i < 8; i++) {
 		for (int j = 0; j < 8; j++) {
 			cells[i][j].x = 40 + j * 80;
@@ -111,33 +248,39 @@ int main() {
 		}
 	}
 
+	// Заполнение поля отображения
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			field[i][j] = 0;
+		}
+	}
+
+	// Начальная позиция курсора
+	field[0][0] = 1;
+	Rectangle(grey, cells[0][0].x, cells[0][0].y, cells[0][0].xEnd, cells[0][0].yEnd);
+
+	// Заполнение цветов для шашек
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 8; j++) {
 
 			if ((i + j) % 2 == 1) {
-				field[i][j] = 1;
-				field[7 - i][7 - j] = 2;
+				cells[i][j].checker = 1;
+				cells[7 - i][7 - j].checker = 2;
 			}
 			else {
-				field[i][j] = 0;
+				cells[i][j].checker = 0;
 			}
 		}
 	}
 
-	/*for (int i = 0; i < 8; i++) { 
-		for (int j = 0; j < 8; j++) {
-			cout << field[i][j];
-		}
-		cout << endl;
-	}*/
+	
+
+	draw();
+	drawCursor();
 
 	while (TRUE) {
 
-		draw();
-		if (_getch() == 13) {
-			system("cls");
-			cin.ignore();
-			return 0;
-		}
+		move();
+		
 	}
 }
